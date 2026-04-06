ac9eb1c5eca827a932ca63bcd0bb1875b57dabd0
Juan Fernando Ramirez D. <juan.ramirez26@uceva.edu.co>
2026-04-06 14:43:26 -0500

    commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Features } from './components/features/features';
+import { Stats } from './components/stats/stats';
+import { Cta } from './components/cta/cta';
+import { Footer } from './components/footer/footer';
+import { Inicio } from './paginas/inicio/inicio';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-  ],
-  bootstrap: [App]
+  declarations: [App, Navbar, Hero, Features, Stats, Cta, Footer, Inicio],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo: 'clientes'},
+  {path: 'clientes', loadChildren: () => import('./clientes/clientes-routing-module').then(m => m.ClientesRoutingModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
